3c566803d22f6d9c101ddc69018565b478e87393
ThiagoReis1 <88296599+ThiagoReis1@users.noreply.github.com>
2024-01-02 11:51:05 -0300

    Update tp1_3.2.py

--- scripts/tp1_3.2.py
+++ scripts/tp1_3.2.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     iniciar.criar_BD()
     criar_tabelas.criar_tabelas()
 
-    filename = 'amazon-meta-sample.txt'
+    filename = 'amazon-meta.txt'
     products = ler_arquivo(filename)
     BD_adicionar_produtos(products)
 
